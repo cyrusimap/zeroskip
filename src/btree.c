cyrusimap/zeroskip

commit 064fa114ff700494d18cf91ea339889de67580ed

btree: Use the shorter length for comparison
I think this is the 'right thing'[tm] to do.

src/btree.c
@@ -519,12 +519,8 @@ unsigned int btree_memcmp(unsigned char *key, size_t keylen,
 
                 b = (unsigned char*)recs[pos]->key;
 
-                if (keylen < recs[pos]->keylen)
-                        goto lessthan;
-                if (keylen > recs[pos]->keylen)
-                        goto greaterthan;
-
-                c = memcmp(k, b, keylen);
+                c = memcmp(k, b, (keylen < recs[pos]->keylen ?
+                                  keylen : recs[pos]->keylen));
                 if (c == 0)
                         goto equals;
                 if (c < 0)

commit a10d9e8551a56a0a1ec9d87e603e892c5700ecc9

btree: Fix the btree_memcmp function.
This reverts commit 064fa114ff700494d18cf91ea339889de67580ed, which
broke the comparator.

src/btree.c
@@ -519,8 +519,12 @@ unsigned int btree_memcmp(unsigned char *key, size_t keylen,
 
                 b = (unsigned char*)recs[pos]->key;
 
-                c = memcmp(k, b, (keylen < recs[pos]->keylen ?
-                                  keylen : recs[pos]->keylen));
+                if (keylen < recs[pos]->keylen)
+                        goto lessthan;
+                if (keylen > recs[pos]->keylen)
+                        goto greaterthan;
+
+                c = memcmp(k, b, keylen);
                 if (c == 0)
                         goto equals;
                 if (c < 0)