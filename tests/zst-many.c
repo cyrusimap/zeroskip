3946f4f83c38be57e1abfad066afef0b9b86339f
Partha Susarla <mail@spartha.org>
2018-08-03 15:29:10 +1000

    tests: Test for adding 4096 records in a db.

---
+++ tests/zst-many.c
@@ -0,0 +1,117 @@
+/*
+ * zeroskip
+ *
+ * zeroskip is free software; you can redistribute it and/or modify
+ * it under the terms of the MIT license. See LICENSE for details.
+ *
+ * Copyright (c) 2017 Partha Susarla <mail@spartha.org>
+ */
+
+#include <libzeroskip/zeroskip.h>
+#include <libzeroskip/macros.h>
+#include <libzeroskip/log.h>
+#include <libzeroskip/util.h>
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define DBNAME "ZSTESTDB"
+
+#define MAXRECS 4095
+
+static int add_records(struct zsdb *db, struct zsdb_txn *txn)
+{
+        size_t i;
+        int ret = ZS_OK;
+
+        /* Begin transaction */
+        ret = zsdb_transaction_begin(db, &txn);
+        if (ret != ZS_OK) {
+                zslog(LOGWARNING, "Could not start transaction!\n");
+                ret = EXIT_FAILURE;
+                goto done;
+        }
+
+        /* Acquire write lock */
+        zsdb_write_lock_acquire(db, 0);
+
+        /* Add records */
+        for (i = 0; i < MAXRECS; i++) {
+                unsigned char key[24], val[24];
+
+                snprintf((char *)key, 8, "key%zu", i);
+                snprintf((char *)val, 8, "val%zu", i);
+
+                ret = zsdb_add(db, key, strlen((char *)key), val,
+                               strlen((char *)val), &txn);
+                if (ret != ZS_OK) {
+                        zslog(LOGWARNING, "Failed adding %s to %s\n",
+                              key, DBNAME);
+                        ret = EXIT_FAILURE;
+                        goto done;
+                }
+        }
+
+        /* Commit the add records transaction */
+        if (zsdb_commit(db, txn) != ZS_OK) {
+                zslog(LOGWARNING, "Failed committing transaction!\n");
+                ret = EXIT_FAILURE;
+                goto done;
+        }
+
+done:
+        /* Release write lock */
+        zsdb_write_lock_release(db);
+
+        /* End the add records transaction */
+        zsdb_transaction_end(&txn);
+
+        return ret;
+}
+
+int main(int argc _unused_, char **argv _unused_)
+{
+        struct zsdb *db = NULL;
+        int ret = EXIT_SUCCESS;
+        struct zsdb_txn *txn = NULL;
+
+        if (zsdb_init(&db, NULL, NULL) != ZS_OK) {
+                zslog(LOGWARNING, "Failed initialising DB.\n");
+                ret = EXIT_FAILURE;
+                goto done;
+        }
+
+        if (zsdb_open(db, DBNAME, MODE_CREATE) != ZS_OK) {
+                zslog(LOGWARNING, "Could not create DB.\n");
+                ret = EXIT_FAILURE;
+                goto done;
+        }
+
+        if (add_records(db, txn) != ZS_OK){
+                ret = EXIT_FAILURE;
+                goto fail1;
+        }
+
+        ret = EXIT_SUCCESS;
+fail1:
+        if (zsdb_close(db) != ZS_OK) {
+                zslog(LOGWARNING, "Could not close DB.\n");
+                ret = EXIT_FAILURE;
+                goto done;
+        }
+
+done:
+        zsdb_final(&db);
+
+        recursive_rm(DBNAME);
+
+        if (ret == EXIT_SUCCESS)
+                printf("add many(): SUCCESS\n");
+        else
+                printf("add many(): FAILED\n");
+
+        exit(ret);
+}
