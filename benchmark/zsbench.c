e340589d28e65d0d011f0bc735f9766b51a37627
Partha Susarla <mail@spartha.org>
2018-09-11 21:14:52 +1000

    benchmark: An initial directory for benchmarking tool.

---
+++ benchmark/zsbench.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+#include <libgen.h>
+
+#include <libzeroskip/macros.h>
+#include <libzeroskip/util.h>
+#include <libzeroskip/version.h>
+#include <libzeroskip/zeroskip.h>
+
+
+int main(int argc, char **argv)
+{
+        exit(EXIT_SUCCESS);
+}
